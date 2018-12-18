f3b3a83ea1e8131a3aaf34600de10c343365be8e
Jan Küster <jkuester@uni-bremen.de>
2018-12-18 10:23:40 +0100

    examples removed unused code

--- examples/templates/helpers/helpers.js
+++ examples/templates/helpers/helpers.js
@@ -1,8 +1,6 @@
 import { Template } from 'meteor/templating'
 import { themeColors } from './shared'
 
-import './codeblock'
-
 const themeColorsList = Object.values(themeColors)
 
 Template.registerHelper('themeColors', function () {
