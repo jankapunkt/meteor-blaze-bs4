3caab05cf03d83b43f9a7fe45e30d66c0c4e976b
Jan Küster <jkuester@uni-bremen.de>
2018-12-18 11:40:12 +0100

    docs updated

--- examples/build.sh
+++ examples/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 meteor npm install -g meteor-build-client
-METEOR_PACKAGE_DIRS=../ meteor meteor-build-client ../build
-
+METEOR_PACKAGE_DIRS=../ meteor meteor-build-client ../docs -t ../build/template.html
+cp ../docs/index.html ../docs/index.md
