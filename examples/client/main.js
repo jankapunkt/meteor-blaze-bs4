9e8581ccee8b18c790b5e8a4535ddc13363d60de
Jan Küster <jkuester@uni-bremen.de>
2018-12-18 10:24:03 +0100

    examples added menu for selecting example templates

--- examples/client/main.js
+++ examples/client/main.js
@@ -1,3 +1,5 @@
+import { ReactiveDict } from 'meteor/reactive-dict'
+import { Template } from 'meteor/templating'
 import 'meteor/jkuester:blaze-bs4'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -10,3 +12,35 @@ import '../templates/breadcrumb/breadcrumb'
 import './main.html'
 
 global.Popper = popper
+
+const allTemplates = [
+  'alerts',
+  'badges',
+  'breadcrumbs'
+]
+
+Template.body.onCreated(function () {
+  const instance = this
+  instance.state = new ReactiveDict()
+  instance.state.set('current', allTemplates[ 0 ])
+})
+
+Template.body.helpers({
+  bstemplates () {
+    return allTemplates
+  },
+  current () {
+    return Template.instance().state.get('current')
+  },
+  isCurrent (name) {
+    return Template.instance().state.get('current') === name
+  }
+})
+
+Template.body.events({
+  'click .template-select' (event, templateInstance) {
+    event.preventDefault()
+    const template = templateInstance.$(event.currentTarget).data('template')
+    templateInstance.state.set('current', template)
+  }
+})
