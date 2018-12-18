7b8a456d8ba4f7b3f6e21c9fdac863a9e8bcbbcd
Jan Küster <jkuester@uni-bremen.de>
2018-12-18 10:07:50 +0100

    examples added breadcrumb template

---
+++ examples/templates/breadcrumb/breadcrumb.js
@@ -0,0 +1,23 @@
+import './breadcrumb.html'
+
+Template.breadcrumbs.helpers({
+  bredcrumbEntries() {
+    return [
+      {
+        title: 'home',
+        active: false,
+        href: '/'
+      },
+      {
+        title: 'examples',
+        active: false,
+        href: '/examples'
+      },
+      {
+        title: 'breadcrumbs',
+        active: true,
+        href: '/examples/breadcrumbs'
+      }
+    ]
+  }
+})
