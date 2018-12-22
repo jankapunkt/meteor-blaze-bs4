598e16aae6359000b0f8f5007fe4ebf968951a29
Jan Küster <jkuester@uni-bremen.de>
2018-12-22 18:57:17 +0100

    published 0.7.0

--- blaze-bs4/package.js
+++ blaze-bs4/package.js
@@ -3,19 +3,19 @@ Package.describe({
   name: 'jkuester:blaze-bs4',
   version: '0.7.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Generic Bootstrap 4 components library for Meteor Blaze.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jankapunkt/meteor-blaze-bs4.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: '../README.md'
 })
 
 Package.onUse(function (api) {
   api.versionsFrom('1.8.0.1')
   api.use('ecmascript')
   api.use('random')
-  api.use('templating')
+  api.use('templating@1.3.2')
   api.addFiles([
     'alert/alert.js',
     'alert/alert.html',
