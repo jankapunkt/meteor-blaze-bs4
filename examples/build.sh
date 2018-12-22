ba9a9690a45b47bc33d9396497013f751c941660
Jan Küster <jkuester@uni-bremen.de>
2018-12-22 17:43:08 +0100

    examples build script moved to npm scripts

--- examples/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-meteor npm install -g meteor-build-client
-METEOR_PACKAGE_DIRS=../ meteor meteor-build-client ../docs -t ../build/template.html
-cp ../docs/index.html ../docs/index.md
