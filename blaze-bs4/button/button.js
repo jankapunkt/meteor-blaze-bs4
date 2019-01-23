2fec9fd81a985cda625e4c20e8643d8843db3169
Jan Küster <jkuester@uni-bremen.de>
2019-01-23 09:53:16 +0100

    button add generic data-* attribtes

--- blaze-bs4/button/button.js
+++ blaze-bs4/button/button.js
@@ -1,6 +1,9 @@
 import { Template } from 'meteor/templating'
+import { dataAttsHelper, onCreatedWithDataAtts } from '../utils/dataAtts'
 import './button.html'
 
-Template.button.events({
+Template.button.onCreated(onCreatedWithDataAtts)
 
+Template.button.helpers({
+  dataAtts: dataAttsHelper
 })
