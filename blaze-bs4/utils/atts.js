ef0d004b12e9d463c271f26012abcd40d920b6ff
Jan Küster <jkuester@uni-bremen.de>
2019-05-02 11:18:56 +0200

    utility class to resolve attributes added+

---
+++ blaze-bs4/utils/atts.js
@@ -0,0 +1,18 @@
+export const defaultAtts = ['title', 'tabindex']
+
+/**
+ * Creates attributes from the instance's data. Only undefined values are not added to the atts,
+ * so null can be used to explicitly turn values off.
+ * @param attsList
+ * @param data
+ */
+export const getAtts = function getAtts (attsList, data) {
+  const atts = {}
+  attsList.forEach(name => {
+    const value = data[ name ]
+    if (typeof value !== 'undefined') {
+      atts[ name ] = value
+    }
+  })
+  return atts
+}
