d784414db770e9b815617f609d669cca2d0af66c
jankapunkt <jkuester@uni-bremen.de>
2019-09-24 16:32:43 +0200

    package remove sync imports to allow full dynamic-import

--- blaze-bs4/blaze-bs4.js
+++ blaze-bs4/blaze-bs4.js
@@ -3,9 +3,6 @@ export const BlazeBs4 = {
     template: 'alert',
     async load () {
       return import('./alert/alert.js')
-    },
-    loadSync () {
-      import './alert/alert.js'
     }
   },
   badge: {
@@ -13,143 +10,109 @@ export const BlazeBs4 = {
     async load () {
       return import('./badge/badge.js')
     },
-    loadSync() {
-      import './badge/badge.js'
-    }
   },
   breadcrumb: {
     template: 'breadcrumb',
     async load () {
       return import('./breadcrumb/breadcrumb.js')
     },
-    loadSync() {
-      import './breadcrumb/breadcrumb.js'
-    }
   },
   button: {
     template: 'button',
     async load () {
       return import('./button/button.js')
     },
-    loadSync() {
-      import './button/button.js'
-    }
+
   },
   card: {
     template: 'card',
     async load () {
       return import('./card/card.js')
     },
-    loadSync() {
-      import './card/card.js'
-    }
+
   },
   collapse: {
     template: 'collapse',
     async load () {
       return import('./collapse/collapse.js')
     },
-    loadSync() {
-      import './collapse/collapse.js'
-    }
+
   },
   dropdown: {
     template: 'dropdown',
     async load () {
       return import('./dropdown/dropdown.js')
     },
-    loadSync() {
-      import './dropdown/dropdown.js'
-    }
+
   },
   item: {
     template: 'item',
     async load () {
       return import('./item/item.js')
     },
-    loadSync() {
-      import './item/item.js'
-    }
+
   },
   jumnbotron: {
     template: 'jumbotron',
     async load () {
       return import('./jumbotron/jumbotron.js')
     },
-    loadSync() {
-      import './jumbotron/jumbotron.js'
-    }
+
   },
   link: {
     template: 'link',
     async load () {
       return import('./link/link.js')
     },
-    loadSync() {
-      import './link/link.js'
-    }
+
   },
   listgroup: {
     template: 'listgroup',
     async load () {
       return import('./listgroup/listgroup.js')
     },
-    loadSync() {
-      import './listgroup/listgroup.js'
-    }
+
   },
   modal: {
     template: 'modal',
     async load () {
       return import('./modal/modal.js')
     },
-    loadSync() {
-      import './modal/modal.js'
-    }
+
   },
   nav: {
     template: 'nav',
     async load () {
       return import('./nav/nav.js')
     },
-    loadSync() {
-      import './nav/nav.js'
-    }
+
   },
   navbar: {
     template: 'navbar',
     async load () {
       return import('./navbar/navbar.js')
     },
-    loadSync() {
-      import './navbar/navbar.js'
-    }
+
   },
   progress: {
     template: 'progress',
     async load () {
       return import('./progress/progress.js')
     },
-    loadSync() {
-      import './progress/progress.js'
-    }
+
   },
   spinner: {
     template: 'spinner',
     async load () {
       return import('./spinner/spinner.js')
     },
-    loadSync() {
-      import './spinner/spinner.js'
-    }
+
   },
   tooltip: {
     template: 'tooltip',
     async load () {
       return import('./tooltip/tooltip.js')
     },
-    loadSync() {
-      import './tooltip/tooltip.js'
-    }
+
   }
 }
