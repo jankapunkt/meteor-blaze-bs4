f4abb7e371dcf9628064a1e62a17aa682545ccbe
jankapunkt <jkuester@uni-bremen.de>
2019-09-24 10:58:51 +0200

    api Routes move all danymic imports to Routes deifnition

--- examples/imports/api/Routes.js
+++ examples/imports/api/Routes.js
@@ -21,90 +21,155 @@ export const Routes = [ {
   template: 'badges',
   name: 'Badge',
   load () {
-    return import('../ui/templates/badge/badge')
+    return [
+      BlazeBs4.badge.load(),
+      import('../ui/templates/badge/badge')
+    ]
   }
 }, {
   template: 'breadcrumbs',
   name: 'Breadcrumb',
   load () {
-    return import('../ui/templates/breadcrumb/breadcrumb')
+    return [
+      BlazeBs4.breadcrumb.load(),
+      import('../ui/templates/breadcrumb/breadcrumb')
+    ]
   }
 }, {
   template: 'buttons',
   name: 'Button',
   load () {
-    return import('../ui/templates/buttons/buttons')
+    return [
+      BlazeBs4.button.load(),
+      import('../ui/templates/buttons/buttons')
+    ]
   }
 }, {
   template: 'buttongroups',
   name: 'Button Group',
   load () {
-    return import('../ui/templates/buttongroups/buttongroups.js')
+    return [
+      BlazeBs4.button.load(),
+      import('../ui/templates/buttongroups/buttongroups.js')
+    ]
   }
 }, {
   template: 'cards',
   name: 'Card',
   load () {
-    return import('../ui/templates/cards/cards')
+    return [
+      BlazeBs4.alert.load(),
+      BlazeBs4.nav.load(),
+      BlazeBs4.link.load(),
+      BlazeBs4.card.load(),
+      BlazeBs4.listgroup.load(),
+      BlazeBs4.item.load(),
+      import('../ui/templates/cards/cards')
+    ]
   }
 }, {
   template: 'collapses',
   name: 'Collapse',
   load () {
-    return import('../ui/templates/collapses/collapses')
+    return [
+      BlazeBs4.alert.load(),
+      BlazeBs4.button.load(),
+      BlazeBs4.card.load(),
+      BlazeBs4.collapse.load(),
+      import('../ui/templates/collapses/collapses')
+    ]
   }
 }, {
   template: 'dropdowns',
   name: 'Dropdown',
   load () {
-    return import('../ui/templates/dropdowns/dropdowns')
+    return [
+      BlazeBs4.dropdown.load(),
+      BlazeBs4.item.load(),
+      import('../ui/templates/dropdowns/dropdowns')
+    ]
   }
 }, {
   template: 'jumbotrons',
   name: 'Jumbotron',
   load () {
-    return import('../ui/templates/jumbotron/jumbotron.js')
+    return [
+      BlazeBs4.jumnbotron.load(),
+      import('../ui/templates/jumbotron/jumbotron.js')
+    ]
   }
 }, {
   template: 'links',
   name: 'Links',
   load () {
-    return import('../ui/templates/links/links')
+    return [
+      BlazeBs4.link.load(),
+      BlazeBs4.tooltip.load(),
+      import('../ui/templates/links/links')
+    ]
   }
 }, {
   template: 'listgroups',
   name: 'List group',
   load () {
-    return import('../ui/templates/listgroups/listgroups')
+    return [
+      BlazeBs4.listgroup.load(),
+      BlazeBs4.item.load(),
+      BlazeBs4.badge.load(),
+      BlazeBs4.alert.load(),
+      import('../ui/templates/listgroups/listgroups')
+    ]
   }
 }, {
   template: 'modals',
   name: 'Modal',
   load () {
-    return import('../ui/templates/modals/modals')
+    return [
+      BlazeBs4.modal.load(),
+      BlazeBs4.button.load(),
+      import('../ui/templates/modals/modals')
+    ]
   }
 }, {
   template: 'navbars',
   name: 'Navbar',
   load () {
-    return import('../ui/templates/navbars/navbars')
+    return [
+      BlazeBs4.navbar.load(),
+      BlazeBs4.nav.load(),
+      BlazeBs4.item.load(),
+      BlazeBs4.collapse.load(),
+      import('../ui/templates/navbars/navbars')
+    ]
   }
 }, {
   template: 'progressbars',
   name: 'Progress',
   load () {
-    return import('../ui/templates/progress/progress')
+    return [
+      BlazeBs4.alert.load(),
+      BlazeBs4.button.load(),
+      BlazeBs4.progress.load(),
+      import('../ui/templates/progress/progress')
+      ]
   }
 }, {
   template: 'spinners',
   name: 'Spinner',
   load () {
-    return import('../ui/templates/spinners/spinners.js')
+    return [
+      BlazeBs4.spinner.load(),
+      BlazeBs4.button.load(),
+      import('../ui/templates/spinners/spinners.js')
+    ]
   }
 }, {
   template: 'tooltips',
   name: 'Tooltips',
   load () {
-    return import('../ui/templates/tooltips/tooltips')
+    return [
+      BlazeBs4.tooltip.load(),
+      import('../ui/templates/tooltips/tooltips')
+      ]
   }
-}]
+} ]
